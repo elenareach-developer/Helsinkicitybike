2ed090e4abe28b176c18450e781f469937b9b470
elenareach-developer <elenareach@gmail.com>
2023-10-12 10:19:10 +0500

    Updates

---
+++ precache-manifest.881df84017baf5571adb706d17020815.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "27edc143f1f1a1a3706e",
+    "url": "/Helsinkicitybike/static/css/main.cf6c7aca.chunk.css"
+  },
+  {
+    "revision": "27edc143f1f1a1a3706e",
+    "url": "/Helsinkicitybike/static/js/main.5baafac8.chunk.js"
+  },
+  {
+    "revision": "feed60521701f45c0198",
+    "url": "/Helsinkicitybike/static/js/runtime~main.9f876e6c.js"
+  },
+  {
+    "revision": "be5b55513632d58c4c6f",
+    "url": "/Helsinkicitybike/static/js/2.53ed4189.chunk.js"
+  },
+  {
+    "revision": "9bd8b746a1d872a60f1b",
+    "url": "/Helsinkicitybike/static/js/3.ae2823c3.chunk.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/Helsinkicitybike/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "19c767f8c41921383d7a78cbf73cab57",
+    "url": "/Helsinkicitybike/index.html"
+  }
+];
